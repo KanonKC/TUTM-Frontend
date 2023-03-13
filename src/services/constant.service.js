1f21a970fdccdba2c6abd993dc203eff61376b68
KanonKC <kanonkce@gmail.com>
2023-03-13 12:32:12 +0700

    Compatible with .env

--- src/services/constant.service.js
+++ src/services/constant.service.js
@@ -1,7 +1,8 @@
 // const dotenv = require('dotenv')
 // dotenv.config()
 // export const BACKEND_URL = "http://localhost:8000"
-export const BACKEND_URL = "http://192.168.0.7:8000"
+// export const BACKEND_URL = "http://192.168.0.7:8000"
 // export const BACKEND_URL = "http://10.2.1.165:8000"
 // export const BACKEND_URL = "http://192.168.68.181:8000"
-// export const BACKEND_URL = `http://${process.env.REACT_APP_DHCP_IPV4}:${process.env.REACT_APP_FRONTEND_PORT}`
+export const BACKEND_URL = `http://${process.env.REACT_APP_DHCP_IPV4}:${process.env.REACT_APP_BACKEND_PORT}`
+console.log(process.env,process.env.REACT_APP_DHCP_IPV4,process.env.REACT_APP_BACKEND_PORT)
