9a4f5776f4b0d72f794f38fc4dd2ed38df9f7b80
KanonKC <kanonkce@gmail.com>
2024-01-06 21:19:12 +0700

    All HTTPS

--- src/services/constant.service.js
+++ src/services/constant.service.js
@@ -1,7 +1,3 @@
 // const dotenv = require('dotenv')
 // dotenv.config()
-// export const BACKEND_URL = "http://localhost:8000"
-// export const BACKEND_URL = "http://192.168.0.7:8000"
-// export const BACKEND_URL = "http://10.2.1.165:8000"
-// export const BACKEND_URL = "http://192.168.68.181:8000"
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
